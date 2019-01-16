11cb6dfbae338d35d5c185530e62b50caa6965eb
mj <madjo85@yandex.ru>
2019-01-16 13:54:00 +0300

    add one more pattern add try with resources

--- src/main/java/TextFileOperations.java
+++ src/main/java/TextFileOperations.java
@@ -5,38 +5,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextFileOperations {
-    private FileReader fileReader;
     private int c;
     private char ch;
+    private String pathFile;
     private List<StringBuffer> textByLine = new ArrayList<StringBuffer>();
     private StringBuffer stringBuffer = new StringBuffer();
 
     public TextFileOperations(String pathFile) throws Exception{
-        fileReader = new FileReader(pathFile);
+        this.pathFile = pathFile;
     }
 
     public List<StringBuffer> readLines() throws IOException {
-        while ((c = fileReader.read()) != -1) {
-            ch = (char) c;
-            if (ch == '\n') {
-                textByLine.add(stringBuffer);
-                stringBuffer = new StringBuffer();
-            } else {
-                stringBuffer.append(ch);
+        try (FileReader fileReader = new FileReader(pathFile)) {
+            while ((c = fileReader.read()) != -1) {
+                ch = (char) c;
+                if (ch == '\n') {
+                    textByLine.add(stringBuffer);
+                    stringBuffer = new StringBuffer();
+                } else {
+                    stringBuffer.append(ch);
+                }
             }
         }
         return textByLine;
     }
 
     public void writeLinesToFile(String pathFile, List<StringBuffer> line) throws Exception{
-        FileWriter fileWriter = new FileWriter(pathFile);
-        for (StringBuffer buffer : line) {
-            for (int i=0; i < buffer.length(); i++) {
-                fileWriter.write(buffer.charAt(i));
+        try (FileWriter fileWriter = new FileWriter(pathFile)) {
+            for (StringBuffer buffer : line) {
+                for (int i = 0; i < buffer.length(); i++) {
+                    fileWriter.write(buffer.charAt(i));
+                }
+                fileWriter.write('\r');
+                fileWriter.write('\n');
             }
-            fileWriter.write('\r');
-            fileWriter.write('\n');
         }
-        fileWriter.close();
     }
 }
